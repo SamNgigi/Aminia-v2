9a86aee8c0afe0a21f885e7014444368b62d2f9e
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-04-25 15:19:04 +0300

    Added google fonts

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom';
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
+import 'jquery/dist/jquery.min.js';
+import 'popper.js/dist/umd/popper.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 import './index.css';
 import App from './App';
