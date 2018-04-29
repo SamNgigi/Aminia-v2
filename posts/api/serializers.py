97f9b2d490795c6bfa42cf14a3d3be1b88f4e65b
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-04-29 09:11:50 +0300

    Added user serializer

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -2,8 +2,39 @@ from rest_framework import serializers
 
 from ..models import Post
 
+from django.contrib.auth.models import User
+
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__'
+
+
+# User serializer
+class CreateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'username',
+            'password'
+        )
+        extra_kwargs = {'password': {'write_only': True}}
+
+        def create(self, validated_data):
+            user = User.objects.create_user(
+                validated_data["username"],
+                None,
+                validated_data['password']
+            )
+            return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'username'
+        )
